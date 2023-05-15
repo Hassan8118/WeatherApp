c6f84100cbb3452ccfe249fce6a0a23c6abbcd05
hassan8118 <mujtabahassan1234567@gmail.com>
2023-05-15 16:17:30 -0400

    WeatherAppDone

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+
+
+string apikey = File.ReadAllText("appsettings.json");
+var key = JObject.Parse(apikey).GetValue("DefaultKey").ToString();
+   
+
+    var client = new HttpClient();
+    var apiKey = "f0479d19c30f27af0b87ab50c66bc66b";
+    var cityName = "Somerset";
+    var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+    var weatherResponse = client.GetStringAsync(weatherUrl).Result;
+    var weatherObject = JObject.Parse(weatherResponse);
+Console.WriteLine(weatherObject);
+
+
